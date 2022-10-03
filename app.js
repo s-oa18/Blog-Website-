277fef0fcae19ec5692a7b54d78cb05aab86e386
SethAmpofo <99514561+s-oa18@users.noreply.github.com>
2022-10-03 02:54:49 +0000

    updated compose route (post functionalities)

--- app.js
+++ app.js
@@ -18,8 +18,13 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+let posts = [];
+
 app.get("/", function (req, res) {
-  res.render("home", { startingContent: homeStartingContent });
+  res.render("home", {
+    startingContent: homeStartingContent,
+    posts: posts,
+  });
 });
 
 app.get("/about", function (req, res) {
@@ -35,7 +40,13 @@ app.get("/compose", function (req, res) {
 });
 
 app.post("/compose", function (req, res) {
-  console.log(req.body.blogTitle);
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody,
+  };
+  posts.push(post);
+
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
